d3ade3ced9b6f68363179008a958efbaf87b7650
Bruno <brunotiecher@hotmail.com>
2024-03-20 19:32:08 -0300

    Menu Admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from galeria.models import Fotografia
 
 def index(request):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "Webbteleescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hublle"}
-    }
-    
-    return render(request, 'galeria/index.html', {"cards": dados})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
